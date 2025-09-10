4289729702bae08b859465c54d72364e9b84547f
Darkness2222 <120744196+Darkness2222@users.noreply.github.com>
2025-09-10 14:05:09 +0900

    Updated Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -64,7 +64,7 @@ export default function Auth() {
         </div>
         
         <h2 className="text-3xl font-bold text-white text-center mb-8">
-          {isSignUp ? 'Sign Up for' : 'Sign In to'}
+          {isSignUp ? 'Sign Up for' : 'Sign In'}
         </h2>
         
         {message && (
